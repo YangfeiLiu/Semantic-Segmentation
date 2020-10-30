197961b280ace01861c2064028398c1bd0f69062
YangfeiLiu <gentleman_lyf@163.com>
2020-10-30 17:11:51 +0800

    update

--- predata.py
+++ predata.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
-from albumentations import RandomResizedCrop
+from albumentations import RandomResizedCrop, RandomCrop
 import sys
 import cv2
 import numpy as np
+from tifffile import imread
+Image.MAX_IMAGE_PIXELS = 1000000000000000
 
 
 color_map = {"背景": [0, 0, 0], "水田": [0, 200, 0], "水浇地": [150, 250, 0], "旱耕地": [150, 200, 150], "园地": [200, 0, 200],
@@ -32,22 +34,30 @@ def cut_data(img, lab, size=512):
     new_h = (h // size) * size if (h // size == 0) else (h // size + 1) * size
     img = np.pad(img, ((0, new_h - h), (0, new_w - w), (0, 0)), mode='constant')
     lab = np.pad(lab, ((0, new_h - h), (0, new_w - w)), mode='constant')
-    aug = RandomResizedCrop(height=size, width=size, scale=(0.8, 1.2))
-    for i in range(0, img.shape[0], size // 4):
+    aug = RandomCrop(height=size, width=size)
+    for i in range(0, img.shape[0], size // 2):
         if i + size > img.shape[0]: break
-        for j in range(0, img.shape[1], size // 4):
+        for j in range(0, img.shape[1], size // 2):
             if j + size > img.shape[1]: break
             img_patch = img[i: i+size, j: j+size, :]
+            img_gray = np.array(Image.fromarray(img_patch).convert('L'))
+            # print(np.sum(img_gray == 0))
+            if np.sum(img_gray == 0) / (size * size) > 0.5:continue
             lab_patch = lab[i: i+size, j: j+size]
-            Image.fromarray(img_patch).save(os.path.join(save_path, 'image', str(cnt) + '.tif'))
-            Image.fromarray(lab_patch).save(os.path.join(save_path, 'label', str(cnt) + '.tif'))
+            if len(np.unique(lab_patch)) == 1: continue
+            Image.fromarray(img_patch).save(os.path.join(save_path, 'image', str(cnt) + '.png'))
+            Image.fromarray(lab_patch).save(os.path.join(save_path, 'label', str(cnt) + '.png'))
             print(cnt)
             cnt += 1
-    for _ in range(1000):
+    for _ in range(5000):
         augment = aug(image=img, mask=lab)
         img_patch, lab_patch = augment['image'], augment['mask']
-        Image.fromarray(img_patch).save(os.path.join(save_path, 'image',  str(cnt) + '.tif'))
-        Image.fromarray(lab_patch).save(os.path.join(save_path,  'label', str(cnt) + '.tif'))
+        img_gray = np.array(Image.fromarray(img_patch).convert('L'))
+        # print(np.sum(img_gray == 0))
+        if np.sum(img_gray == 0) / (size * size) > 0.5: continue
+        if len(np.unique(lab_patch)) == 1: continue
+        Image.fromarray(img_patch).save(os.path.join(save_path, 'image',  str(cnt) + '.png'))
+        Image.fromarray(lab_patch).save(os.path.join(save_path,  'label', str(cnt) + '.png'))
         print(cnt)
         cnt += 1
 
@@ -67,23 +77,24 @@ def split_train_val():
 
 
 if __name__ == '__main__':
-    save_path = '/media/hb/d2221920-26b8-46d4-b6e5-b0eed6c25e6e/train/'
-    # root = ''
-    # items = os.listdir(root)
-    # labels = [x for x in items if 'label' in x]
-    # images = [x for x in items if x not in labels]
-    # os.makedirs(save_path, exist_ok=True)
-    # a = list(zip(*zip(images, labels)))
-    # cnt = 1
+    save_path = '/media/hp/1500/liuyangfei/huawei'
+    root = '/media/hp/1500/liuyangfei/data/road_data/huawei'
+    items = os.listdir(root)
+    labels = [x for x in items if 'label' in x]
+    images = [x for x in items if x not in labels]
+    os.makedirs(save_path, exist_ok=True)
+    a = list(zip(*zip(images, labels)))
+    cnt = 1
     # ratio = [0] * 16
-    # for i in range(len(a[0])):
-    #     image = np.array(Image.open(os.path.join(root, a[0][i])))
-    #     # plt.imshow(image[:, :, 1:])
-    #     # plt.show()
-    #     label = np.array(Image.open(os.path.join(root, a[1][i])).convert('L'))
-    #     label = change(label)
-    #     for j in range(16):
-    #         ratio[j] += np.sum(label == j)
+    for i in range(len(a[0])):
+        image = np.array(Image.open(os.path.join(root, a[0][i])))
+        # image = imread(os.path.join(root, a[0][i]))
+        label = np.array(Image.open(os.path.join(root, a[1][i])).convert('L'))
+        # print(np.unique(label))
+        # label = imread(os.path.join(root, a[1][i]))
+        # label = change(label)
+        # for j in range(16):
+        #     ratio[j] += np.sum(label == j)
     # print(ratio)
-    #     # cut_data(image, label, 512)
+        cut_data(image, label, 512)
     split_train_val()
